ceaf1a028260b66d55c19962c9fe47b10f77547e
kiggz <nico.peignot@outlook.fr>
2022-03-15 23:11:44 +0100

    number order & total price/articles

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,8 @@
+let calculPrice = []
+let calculArticles = []
+let fetchPromises = []
+
+// création du tableau qui contenir l'ensemble des prix 
 cart = JSON.parse(localStorage.getItem('cart')) 
 console.table(cart)
 // tableau article = ProductId | color | numberProduct
@@ -6,10 +11,9 @@ for(let i = 0; i < cart.length; i++) {
   let articleId = cartProduct.ProductId
   let colorChosen = cartProduct.color
  
-  fetch(`http://localhost:3000/api/products/${articleId}`)
+  const fetchPromise = fetch(`http://localhost:3000/api/products/${articleId}`)
     .then((res) => res.json()) 
     .then((value) => {
-      
       // ==/ Création de l'article 
       let blockItems = document.getElementById('cart__items')
       let article = document.createElement('article')
@@ -50,8 +54,9 @@ for(let i = 0; i < cart.length; i++) {
       blockItemContentDescription.appendChild(articleColor)
       
       // ==/==/==/==/ Prix de l'article
+      let totalProductPrice = value.price*cartProduct.numberProduct
       let articlePrice = document.createElement("p")
-      articlePrice.innerText = `${value.price} €`
+      articlePrice.innerText = `${totalProductPrice} €`
       blockItemContentDescription.appendChild(articlePrice)
 
       // ==/==/==/==/ Block settings
@@ -95,47 +100,61 @@ for(let i = 0; i < cart.length; i++) {
       deleteItem.innerText = "Supprimer"
       blockItemContentSettingsDelete.appendChild(deleteItem)
       deleteItem.addEventListener("click", function(e) { 
-
         cart.splice(1, i)
         const cartString = JSON.stringify(cart)
         localStorage.removeItem('cart')
         localStorage.setItem('cart', cartString)   
-        }) 
+      }) 
       //////////////////////////////////////////////
-
-     total = cart.reduce(x => value.price)
-
-     console.log(total)
-
+      //
+      calculPrice.push(totalProductPrice) 
+        
+      //
       ///////////////////////////////////////////
       
     })
     .catch(function(err) {
       console.log(err)
     })
+// création d'un tableau fetch pour récuper les données totales après leurs éxécution
+  fetchPromises.push(fetchPromise)
+  console.log(fetchPromises)
 }
 
-///////////////////////////////////////////////
+//// Affichage du total de la commande qui attends la réalisation des promesses 
+//Affichage de la quantité d'articles
+blockQuantity = document.getElementById("totalQuantity")
+blockQuantity.innerText = cart.length
+//Affichage du prix total 
+async function waitFetch() {
+  await Promise.all(fetchPromises) 
+  for (let i = 0; i < fetchPromises.length; i++) {
+    let totalPrice = calculPrice.reduce((a, b)=> a + b,0)   
+    blockPrice = document.getElementById("totalPrice")
+    blockPrice.innerText = totalPrice
+  }
+}
+waitFetch()
 
 ///récupération des id du panier
-
 let productId = cart.map(x => x.ProductId)
 console.log(productId)
 
-
-////////////////////////////////////////////////////
-
-
+// récupération du message d'erreur
 function getInputError(message, errorMessage){
   document
     .getElementById(message)
     .innerText = errorMessage}
 
+//création des filtre de validation 
 let firstNameInputValidator = undefined;
 let lastNameInputValidator = undefined;
 let adressInputValidator = undefined;
 let cityInputValidator = undefined;
 let emailInputValidator = undefined;
+
+// Paramétrage des input
+
 //====FIRSTNAME
 let firstNameInput = document.getElementById("firstName")
 firstNameInput.addEventListener("change", function(e){
@@ -224,8 +243,6 @@ submit.addEventListener("click", function(e) {
     let customerEmail = email.value
     
     //création de l'objet body contetnant les data du form et les éléments du panier
-    
-    
     const order = {
       contact : {
         "firstName" : customerFirstName,
@@ -241,7 +258,6 @@ submit.addEventListener("click", function(e) {
     console.log(order)
    
     //envoies de l'objet vers le serveur  
-    //////////////////////////////////////////
     fetch("http://localhost:3000/api/products/order", {
 	    method: "POST",
 	    headers: { 
@@ -250,29 +266,21 @@ submit.addEventListener("click", function(e) {
       },  
       body: JSON.stringify(order)
     })
-      //récupération de la réponse
-      .then(function(res) {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      //récupération de la valeur et envoi vers le localstorage
-      .then(value => {
-        localStorage.removeItem('cart')
-        orderResponse = JSON.stringify(value.orderId)
-        localStorage.setItem("orderNumber", orderResponse);
-        submit.location.href="http://localhost:5500/html/confirmation.html"
-        e.stopPropagation()
-      })
-      .catch(function(err) {
-        console.log(err)
-      })
+    //récupération de la réponse
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    //récupération de la valeur et envoi vers le localstorage
+    .then(value => {
+      localStorage.removeItem('cart')
+      orderResponse = JSON.stringify(value.orderId)
+      localStorage.setItem("orderNumber", orderResponse);
+      document.location.href="http://localhost:5500/html/confirmation.html"
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
   }
 })
-
-
-
-
-
-
-// créer un numéro de ticket via "Math.random"
--- js/confirmation.js
+++ js/confirmation.js
@@ -1,13 +1,4 @@
-
-
-
-
-
-
-
-
-/*
-
-
-
-*/
+const getOrderId = localStorage.getItem("orderNumber")
+console.log(getOrderId)
+const cardNumberOrder= document.getElementById('orderId') 
+cardNumberOrder.innerText =  getOrderId
